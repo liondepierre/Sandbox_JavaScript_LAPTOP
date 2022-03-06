83bf83711f1c9d007be7e533c86e1bcb486b2d4b
liondepierre <mertaslantas@outlook.com>
2022-03-06 19:17:19 +0100

    first commit

---
+++ lektier-1/lektier1.js
@@ -0,0 +1,16 @@
+// const expression = /^https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z]{1,3}$/;
+
+// const links = document.getElementsByTagName('li');
+
+// for(const element of links) {
+//     const text = element.innerText;
+
+//     if(text.match(expression)) {
+//         const a = document.createElement('a');
+//         a.href = text;
+//         a.appendChild(document.createTextNode(text));
+
+//         element.firstChild.remove();
+//         element.appendChild(a);
+//     }
+// }
